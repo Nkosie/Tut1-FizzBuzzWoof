adae5d1845cab9850496d119054c9b364f89f51b
Nkoskhona <nk0skh0n2@live.com>
2016-02-20 11:34:16 +0200

    check 1

---
+++ tut1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b,i, c ,v;
+	a = 3;
+	b = 5;
+	c = 7;
+	for (i = 1; i = 2ch
+	{
+		v = i;
+		if( i % a = 0)
+		{
+			cout << "Fizz" << endl;
+		}
+		if (i % b = 0)
+		{
+			cout << "Buzz" << endl;
+		}
+		if (i % a =0  && i%b =0)
+		{
+			cout << "Fizz Buzz" << endl;
+		}
+		else
+		{
+			cout << "%d, i" << endl;
+		}
+	}
+}
